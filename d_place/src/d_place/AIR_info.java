d4bf4c385b79dd37f58e44762aa9c2a17d9c4815
mana1001 <timuncleuncle@hotmail.com>
2013-11-26 19:33:30 +0800

    AIR_info.java -add comment -delect show_info bug

--- d_place/src/d_place/AIR_info.java
+++ d_place/src/d_place/AIR_info.java
@@ -22,36 +22,16 @@ public class AIR_info {
 
 	public void download_info(String url , String path)throws IOException, ConnectException
 	{
+		//download air_info.json with url
 	    InputStream is = new URL(url).openConnection().getInputStream();
-	    
-	   // FileOutputStream fos = new FileOutputStream(path);
-	
-	    //fos.close();
-	    /*
-	    StringBuilder inputStringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
-        String line = bufferedReader.readLine();
-        BufferedWriter bw = new BufferedWriter(new FileWriter(path));
-        
-        while(line != null){
-            inputStringBuilder.append(line);inputStringBuilder.append('\n');
-            line = bufferedReader.readLine();
-            if(line != null)
-            bw.write(line);
-        }
-        bw.flush();
-        bw.close();   
-	    
-	    is.close();
-	    */
 	    try {
-            
+            //use bufferedrwader get buffer length and get the json with UTF-8
             BufferedReader in = new BufferedReader(new InputStreamReader((new URL(url).openStream()), "UTF-8"));
+            //write the buffer in json file
             BufferedWriter out = new BufferedWriter(new FileWriter(path));
             char[] cbuf=new char[255];
-            while ((in.read(cbuf)) != -1) {
-                out.write(cbuf);
-            }
+            //get data and write data
+            for (int length; (length = in.read(cbuf)) > 0; out.write(cbuf, 0, length));
             in.close();
             out.close();
 
@@ -65,12 +45,13 @@ public class AIR_info {
 	}
 	public void show_info(String path) throws FileNotFoundException, JSONException
 	{
+		//read json
 		JSONArray air_info = new JSONArray(new JSONTokener(new FileReader(new File(path))));
 		JSONObject air_object ;
+		//print all json data
 		for(int i = 0 ; i < air_info.length() ; i ++)
 		{
 			air_object = air_info.getJSONObject(i);
-			if(air_object.get("SiteName").equals("³Á¼d"))
 			System.out.println(air_object.get("SiteName") + "  "+ air_object.get("MonitorDate") + "  " + air_object.get("PSI"));
 		}
 	}
