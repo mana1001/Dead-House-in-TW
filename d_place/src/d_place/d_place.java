7643d5d42be72d4b2f8a1074efbff58521fe0458
mana1001 <timuncleuncle@hotmail.com>
2013-11-27 23:09:24 +0800

    d_place.java -change download path

--- d_place/src/d_place/d_place.java
+++ d_place/src/d_place/d_place.java
@@ -16,7 +16,7 @@ public class d_place {
 		//set the download url
 		String air_url = "http://opendata.epa.gov.tw/ws/Data/AQXDaily/?$orderby=MonitorDate%20desc&$skip=0&$top=1000&format=json";
 		//set the new file save path
-		String air_path = "C:/Users/Timuncle/Desktop/AIR_info.json";
+		String air_path = "AIR_info.json";
 		//create a new object
 		AIR_info air = AIR_info.getObject();
 		//download the air information
