5c2ec5fbc4656b73a2a26e271bf2d93846130b86
yongxiang <p19920310@gmail.com>
2013-12-30 16:16:56 +0800

    death_info

---
+++ death_info/src/county.java
@@ -0,0 +1,14 @@
+
+public class county {
+	
+	public int air;
+	public int water;
+	public int all_cause;
+	public double air_rate;
+	public double water_rate;
+	county()
+	{
+		air = water =  0;
+		all_cause = 1 ;
+	}
+}
